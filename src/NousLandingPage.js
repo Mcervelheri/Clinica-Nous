204a8ada9076c8b83f6dccb18cce224549a2987e
Matheus Vinicius Cervelheri <mcervelheri@gmail.com>
2025-01-25 21:43:50 -0300

    significado

--- src/NousLandingPage.js
+++ src/NousLandingPage.js
@@ -62,9 +62,9 @@ const NousLandingPage = () => {
                 <div className="container mx-auto text-center">
                     <h2 className="text-3xl font-bold mb-6 text-[#5e4031]">NOUS: Significado</h2>
                     <p className="max-w-2xl mx-auto text-lg">
-                        NOUS, na filosofia grega, representa a inteligência, a mente e a razão.
-                        Em nossa clínica, simboliza o entendimento profundo do ser humano,
-                        valorizando a complexidade da mente e a importância do autoconhecimento.
+                        O nome Nous vem do grego antigo e significa “mente”, “inteligência” ou “razão”. Na filosofia grega, Nous é a razão divina que governa o universo, trazendo ordem e harmonia ao cosmos. Para nós, esse conceito simboliza a importância do autoconhecimento e da inteligência emocional no cuidado com a saúde mental.
+                        <br></br><br></br>
+                        Em nossa clínica, Nous representa nossa missão de cuidar da mente e promover o equilíbrio através da razão e do entendimento profundo dos sentimentos e pensamentos.
                     </p>
                 </div>
             </section>
